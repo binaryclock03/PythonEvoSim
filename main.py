497cf0316ba7adbc318429e0eb524c14b7ace235
binaryclock03 <binaryclock03@gmail.com>
2022-05-23 13:16:32 -0400

    test

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ def convert_coordinates(point):
     return point[0], 800-point[1]
 
 class Joint():
-    def __init__(self, position, radius, elasticity, friction, density):
+    def __init__(self, position, radius):
         self.radius = radius
         self.body = pymunk.Body()
         self.body.position = position
         self.shape = pymunk.Circle(self.body, radius)
-        self.shape.elasticity = elasticity
+        self.shape.elasticity = 1
         self.shape.friction = 1
-        self.shape.density = density
+        self.shape.density = 1
     
     def draw(self, display):
         x, y = convert_coordinates(self.body.position)
@@ -33,10 +33,10 @@ class Limb():
     def __init__(self, joint1, joint2):
         self.joint1 = joint1
         self.joint2 = joint2
-        self.joint = pymunk.PinJoint(self.joint1.body, self.joint2.body) 
+        self.joint = pymunk.PinJoint(self.joint1.body, self.joint2.body, (0,20)) 
 
     def draw(self, display):
-        pygame.draw.line(display, (0,0,0), convert_coordinates(self.joint1.body.position), convert_coordinates(self.joint2.body.position), 2)
+        pygame.draw.line(display, (0,0,0), convert_coordinates(self.joint1.body.position + self.joint.anchor_a), convert_coordinates(self.joint2.body.position + self.joint.anchor_b), 2)
     
     def addToSpace(self, space):
         space.add(self.joint)
@@ -47,8 +47,8 @@ class Creature():
         self.joints = []
         self.limbs = []
 
-    def addJoint(self, position, radius, elasticity, friction, density):
-        self.joints.append(Joint(position, radius, elasticity, friction, density))
+    def addJoint(self, position, radius):
+        self.joints.append(Joint(position, radius))
 
     def addLimb(self, jointindex1, jointindex2):
         self.limbs.append(Limb(self.joints[jointindex1],self.joints[jointindex2]))
@@ -73,7 +73,7 @@ class Wall():
         self.body = pymunk.Body(body_type=pymunk.Body.STATIC)
         self.shape = pymunk.Segment(self.body, point1, point2, thickness/2)
         self.shape.elasticity = 1
-        self.shape.friction = 0
+        self.shape.friction = 1
     
     def draw(self, display):
         pygame.draw.line(display, (0,255,0), convert_coordinates(self.point1), convert_coordinates(self.point2), self.thickness)
@@ -83,34 +83,29 @@ class Wall():
 
 def game():
     creature = Creature()
-    creature.addJoint((400,400), 10, 1.2, 1, 1)
-    creature.addJoint((420,400), 10, 1.1, 1, 1)
+    creature.addJoint((400,400), 10)
+    creature.addJoint((420,400), 10)
     creature.addLimb(0,1)
     creature.addToSpace(space)
 
     creature2 = Creature()
-    creature2.addJoint((300,400), 10, 1.1, 1, 1)
-    creature2.addJoint((320,400), 10, 1.1, 1, 1)
-    creature2.addJoint((350,410), 10, 1.1, 1, 1)
+    creature2.addJoint((290,400), 10)
+    creature2.addJoint((320,400), 10)
+    creature2.addJoint((350,410), 10)
     creature2.addLimb(0,1)
     creature2.addLimb(1,2)
-    creature2.addLimb(0,2)
     creature2.addToSpace(space)
 
-    creature3 = Creature()
-    creature3.addJoint((500,500), 50, 1.01, 1, 1)
-    creature3.addToSpace(space)
-
-    floor = Wall((0,10), (800,10), 100)
+    floor = Wall((0,10), (800,10), 200)
     floor.addToSpace(space)
 
-    wallright = Wall((800,0), (800,800), 10)
+    wallright = Wall((800,0), (800,800), 200)
     wallright.addToSpace(space)
 
-    wallleft= Wall((0,0), (0,800), 10)
+    wallleft= Wall((0,0), (0,800), 200)
     wallleft.addToSpace(space)
 
-    roof = Wall((0,800), (800,800), 100)
+    roof = Wall((0,800), (800,800), 200)
     roof.addToSpace(space)
 
     while True:
@@ -120,7 +115,6 @@ def game():
         display.fill((255,255,255))
         creature.draw(display)
         creature2.draw(display)
-        creature3.draw(display)
         floor.draw(display)
         wallright.draw(display)
         wallleft.draw(display)
