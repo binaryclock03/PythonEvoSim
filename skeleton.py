ace13e7460f8950cbb88cfb230055e3fa57b9c6f
AntoineBurnt <antoinebrule@cmail.carleton.ca>
2022-05-23 13:25:36 -0400

    Update skeleton.py

--- skeleton.py
+++ skeleton.py
@@ -1,19 +1,22 @@
 import random as random
 
-points = []
-links = []
-numPoints = 4
-
-#Create List of Points
-for x in range(numPoints):
-    points.append((random.random(),random.random()))
-
-#Generate All Possible Links
-for x in range(numPoints):
-    for y in range(x+1,(numPoints)):
-        links.append((x,y))
-
-#Simplification
-for x in range(numPoints-3):
-    links.pop(random.randrange(0,len(links)))
+def genSkeleton(numPoints,scale):
+    
+    points = []
+    links = []
+    
+    #Create List of Points
+    for x in range(numPoints):
+        points.append((random.random()*scale,random.random()*scale))
+    
+    #Generate All Possible Links
+    for x in range(numPoints):
+        for y in range(x+1,(numPoints)):
+            links.append((x,y))
+    
+    #Simplification
+    for x in range(numPoints-3):
+        links.pop(random.randrange(0,len(links)))
+        
+    return points,links
 
