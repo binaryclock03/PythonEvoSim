a958b49993b61ac5431e6b059449d97fab1a0d20
binaryclock03 <binaryclock03@gmail.com>
2022-05-23 10:40:15 -0400

    added creature, limb, and joint class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pymunk
 import pygame
+from sympy import rad
 
 pygame.init()
 
@@ -12,30 +13,74 @@ FPS = 50
 def convert_coordinates(point):
     return point[0], 800-point[1]
 
-body = pymunk.Body()
-body.position = (400, 400)
-shape = pymunk.Circle(body, 10)
-shape.density = 1
-shape.elasticity = 1.1
-space.add(body, shape)
+class Joint():
+    def __init__(self, position, radius, elasticity, friction, density):
+        self.radius = radius
+        self.body = pymunk.Body()
+        self.body.position = position
+        self.shape = pymunk.Circle(self.body, radius)
+        self.shape.elasticity = elasticity
+        self.shape.friction = 1
+        self.shape.density = density
+    
+    def draw(self, display):
+        x, y = convert_coordinates(self.body.position)
+        pygame.draw.circle(display, (255, 0 , 0), (int(x), int(y)), self.radius)
 
-segment_body = pymunk.Body(body_type=pymunk.Body.STATIC)
-segment_shape = pymunk.Segment(segment_body, (0,50), (800,100), 5)
-segment_shape.elasticity = 1
-space.add(segment_body, segment_shape)
+    def addToSpace(self, space):
+        space.add(self.body, self.shape)
 
+class Limb():
+    def __init__(self, joint1, joint2):
+        self.joint1 = joint1
+        self.joint2 = joint2
+        body = pymunk.Body()
+        body.position = ((self.joint1.body.position + self.joint2.body.position) / 2)
+        shape = pymunk.Segment(self.body, self.joint1.body.position, self.joint2.body.position, 1)
+
+    def draw(self, display):
+        pygame.draw.line(display, (0,0,0), self.joint1.body.position, self.joint1.body.position, 10)
+    
+    def addToSpace(self):
+        space.add(self.body, self.shape)
+
+
+class Creature():
+    def __init__(self):
+        self.joints = []
+        self.limbs = []
+
+    def addJoint(self, position, radius, elasticity, friction, density):
+        self.joints.append(Joint(position, radius, elasticity, friction, density))
+
+    def addLimb(self):
+        self.limbs.append(Limb(1,1))
+
+    def addToSpace(self, space):
+        for joint in self.joints:
+            joint.addToSpace(space)
+        for limb in self.limbs:
+            limb.addToSpace(space)
+
+    def draw(self, display):
+        for joint in self.joints:
+            joint.draw(display)
+        for limb in self.limbs:
+            limb.draw(display)
+    
 
 
 def game():
+    creature = Creature()
+    creature.addJoint((400,400), 5, 0, 1, 1)
+    creature.addJoint((420,400), 5, 0, 1, 1)
+    creature.addToSpace(space)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         display.fill((255,255,255))
-        x, y = convert_coordinates(body.position)
-        pygame.draw.circle(display, (255, 0 , 0), (int(x), int(y)), 10)
-        pygame.draw.line(display, (0,0,0), convert_coordinates((0,50)), convert_coordinates((800,100)), 10)
-
+        creature.draw(display)
         pygame.display.update()
         clock.tick(FPS)
         space.step(1/FPS)
