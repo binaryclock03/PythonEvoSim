ebde381e82c5ad7caebd92a5dfe4acf715447022
AntoineBurnt <antoinebrule@cmail.carleton.ca>
2022-05-24 12:56:14 -0400

    I just want to pull

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import simObjects as so
+import populationManager as pm
 
-print(so.simNoGraphics(10, 10))
+
+testPop = pm.loadPop("testing",0)
+
+for x in range(10):
+
+    testPop.nextGenertation(so.sim(30,creatureList=testPop.creatures))
+
+    testPop.savePop()
+
+
+
+#print(so.simNoGraphics(10, 10))
