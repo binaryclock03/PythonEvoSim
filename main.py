093d2aa46626ab271a1e3349c9092c619e428f5d
binaryclock03 <binaryclock03@gmail.com>
2022-05-24 19:51:40 -0400

    fast sim now only runs creature instead of sample

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ testPop.savePop()
 testPop = pm.loadPop("fastTest",0)
 
 for x in range(1000):
-    testPop.nextGenertation(so.fastsimthing(10,creatureList=testPop.creatures))
+    testPop.nextGenertation(so.fastsim(10, creatureList=testPop.creatures, TPS = 60))
     testPop.savePop()
