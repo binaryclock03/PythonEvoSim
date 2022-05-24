c3ae5a40c1de4793b93d82488c48e4b3fd7fb3a7
binaryclock03 <binaryclock03@gmail.com>
2022-05-24 16:36:06 -0400

    started gui.py (lol)

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import simObjects as so
 import populationManager as pm
 
-testPop = pm.loadPop("testing",0)
+testPop = pm.Population("name")
+testPop.addRandomCreatures(100)
+testPop.savePop()
+testPop = pm.loadPop("name",0)
 
-for x in range(1000):
+for x in range(100):
 
-    testPop.nextGenertation(so.sim(30,creatureList=testPop.creatures,graphics=False))
+    testPop.nextGenertation(so.sim(10,creatureList=testPop.creatures,graphics=False))
 
     testPop.savePop()
-
