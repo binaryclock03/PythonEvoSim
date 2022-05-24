30d29605ce3c22ddc12f0b40981728fbcf99137d
AntoineBurnt <antoinebrule@cmail.carleton.ca>
2022-05-24 15:42:31 -0400

    Fixes n stuff

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import simObjects as so
 import populationManager as pm
 
-
 testPop = pm.loadPop("testing",0)
 
-for x in range(10):
+for x in range(1000):
 
-    testPop.nextGenertation(so.sim(5,creatureList=testPop.creatures))
+    testPop.nextGenertation(so.sim(30,creatureList=testPop.creatures,graphics=False))
 
     testPop.savePop()
+
