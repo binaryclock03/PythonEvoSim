281b6749569f033522bc8cb3ed70ad52b59240d7
binaryclock03 <binaryclock03@gmail.com>
2022-05-24 18:19:33 -0400

    moved flatness check to end of generation, improving sim time

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import simObjects as so
 import populationManager as pm
 
 testPop = pm.Population("fastTest")
-testPop.addRandomCreatures(10)
+testPop.addRandomCreatures(1000)
 testPop.savePop()
 testPop = pm.loadPop("fastTest",0)
 
-for x in range(100):
+for x in range(1000):
     testPop.nextGenertation(so.fastsimthing(10,creatureList=testPop.creatures))
     testPop.savePop()
