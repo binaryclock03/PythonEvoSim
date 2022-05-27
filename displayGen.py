46ae5cfef50419c0954f30a5dbed5c5381d55007
AntoineBurnt <antoinebrule@cmail.carleton.ca>
2022-05-26 22:39:54 -0400

    other fix

--- displayGen.py
+++ displayGen.py
@@ -2,6 +2,6 @@ import simObjects as so
 import populationManager as pm
 import simulations as sim
 
-testPop = pm.Population("genomeTest")
-testPop.addRandomCreatures(1)
-sim.playback(0, testPop.creatures)
+testPop = pm.loadPopTest("seedingTest", 0)
+#sim.playback(0, testPop.getPreview())
+testPop.renamePop('saveTest')
