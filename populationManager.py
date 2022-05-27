c9f846df4d149a4c2d9d4a7ef2721cfbf704c78b
AntoineBurnt <antoinebrule@cmail.carleton.ca>
2022-05-27 13:40:38 -0400

    actually fixed the summary saving

--- populationManager.py
+++ populationManager.py
@@ -499,7 +499,7 @@ class Population():
              f.close()
 
         #Write fitness list to cvs file
-        f = open("Populations\\"+ self.popName + '\\' + self.popName + "_summary.csv", 'r')
+        f = open("Populations\\"+ self.popName + '\\' + self.popName + "_summary.csv", 'a')
         csv.writer(f).writerow(fitnessList)
         f.close()
 
